6feeba05657c4bc9c95e228faf6c909a938259ba
Joey Harris <jharris@eaglemetal.com>
2025-09-20 17:06:37 -0400

    take out Compodium

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import vue from "@vitejs/plugin-vue";
 import laravel from "laravel-vite-plugin";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
-import { compodium } from "@compodium/vue";
-import path, { resolve } from "node:path";
+import path from "node:path";
 export default defineConfig({
   plugins: [
     laravel({
@@ -39,35 +38,6 @@ export default defineConfig({
       },
     }),
     vueDevTools(),
-    compodium({
-      /* Customize the directories where components are discovered */
-      componentDirs: [{ path: "resources/js/components", pathPrefix: false }],
-
-      /* Customize compodium's base directory. */
-      dir: "compodium/",
-
-      /**
-       * Configure your application's mainPath
-       */
-      mainPath: "resources/js/app.ts",
-
-      /* List of glob patterns to ignore components */
-      ignore: [],
-
-      /* Whether to include default collections for third-party libraries. */
-      includeLibraryCollections: true,
-
-      extras: {
-        /*
-         * Customize Compodium's UI Colors.
-         * See: https://ui.nuxt.com/getting-started/theme#colors for acceptable values
-         */
-        colors: {
-          primary: "green",
-          neutral: "zinc",
-        },
-      },
-    }),
   ],
   resolve: {
     alias: {

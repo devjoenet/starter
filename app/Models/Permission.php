7036c729e50b1477b447dc2d929448e848f11bc2
Joey Harris <devjoenet@gmail.com>
2026-01-21 01:57:09 -0500

    add perms and roles

---
+++ app/Models/Permission.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Spatie\Permission\Contracts\Permission as PermissionContract;
+use Spatie\Permission\Models\Permission as BasePermission;
+
+class Permission extends BasePermission implements PermissionContract
+{
+    //
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Spatie\Permission\Contracts\Role as RoleContract;
+use Spatie\Permission\Models\Role as BaseRole;
+
+class Role extends BaseRole implements RoleContract
+{
+    //
+}
